9e54de34e2694cbe15fef8ed5fc1c84a472c7efa
Hsiang <1207victory@gmail.com>
2014-09-25 13:35:22 +0800

    Add 3th lib into vendor and require

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery-ui
+//= require moment
+//= require bootstrap 
+//= require fullcalendar 
